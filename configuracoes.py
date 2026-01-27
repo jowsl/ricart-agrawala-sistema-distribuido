0fdb347e09e06427b4d42400c2ed0400a9e31fd4
Jouberth <jouberthsl@hotmail.com>
2026-01-26 22:49:57 -0300

    config implementado

---
+++ configuracoes.py
@@ -0,0 +1,9 @@
+NOS = [
+        #VM 1
+    {"id": 1, "ip": "10.0.0.120", "p2p_port": 5001, "client_port": 6001},
+    {"id": 2, "ip": "10.0.0.120", "p2p_port": 5002, "client_port": 6002},
+    {"id": 3, "ip": "10.0.0.120", "p2p_port": 5003, "client_port": 6003},
+        #VM 2
+    {"id": 4, "ip": "10.0.0.19",  "p2p_port": 5004, "client_port": 6004},
+    {"id": 5, "ip": "10.0.0.19",  "p2p_port": 5005, "client_port": 6005}
+]
